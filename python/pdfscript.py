8cf20e8557f0534cb37ec3ac94e06f411dd3e308
Julin S <julinshaji01@gmail.com>
2022-09-15 02:39:38 +0530

    [python] slight edits to pdfscript

--- python/pdfscript.py
+++ python/pdfscript.py
@@ -1,7 +1,5 @@
 """
-Requires Python>=3.7
-
-(Relies on dicts being ordered)
+Requires Python>=3.6
 """
 
 import argparse
@@ -14,10 +12,16 @@ import tempfile
 select pages: gs
 crop borders: pdfcrop
 landscape/portrait: pdfnup
+
+cmds:
+ - gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sPageList=$ARG -sOutputFile=$INFILE $OUTFILE
+ - pdfcrop --margins $ARG $INFILE $OUTFILE
+ - ./pdfnup --nup $LAYOUT $INFILE -o $OUTFILE --landscape
 """
 
 CMDS = {
-    "gs": "{gs} -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sPageList={pages} sOutputFile={inp} {outp}",
+    "gs": ("{gs} -sDEVICE=pdfwrite -dNOPAUSE -dBATCH"
+           "-sPageList={pages} sOutputFile={inp} {outp}"),
     "pdfcrop": "{pdfcrop} --margins {margins} {inp} {outp}",
     "pdfnup": "{pdfnup} --nup {layout} {inp} -o {outp} {landscape}"
 }
@@ -44,74 +48,35 @@ def dispatch_cmd(op: str, args, inp: str, outp: str) -> None:
                                     inp=inp,
                                     outp=outp,
                                     landscape=landscape)
-    #subprocess.run(cmd.split())
-    print(cmd)
-
-# gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sPageList=$PAGES -sOutputFile=$INFILE $SELECTED
-# pdfcrop --margins $MARGINS $SELECTED $CROPPED
-# ./pdfnup --nup $ARG $INFILE -o $OUTFILE [--landscape]
-
-
-"""
-https://docs.python.org/3/library/subprocess.html
-https://docs.python.org/3/library/tempfile.html
-
-cmds:
- - gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -sPageList=$ARG -sOutputFile=$INFILE $OUTFILE
- - pdfcrop --margins $ARG $INFILE $OUTFILE
- - ./pdfnup --nup $LAYOUT $INFILE -o $OUTFILE --landscape
----
-
-Check which all ops are active
-There are flags for each op: o1, o2, o3
-
-Seleted ops = selops:list 
-tmp1 = temp()
-tmp2 = temp()
-files = [fin, tmp1, tmp2, fout]
-infile = 0
-outfile = -1
-
-
-while selops:
-    print(f"{infile} -> {outfile}")
-    infile = outfile
-    if outfile == 1:
-        outfile = 2
+    if args.dry:
+        print(cmd)
     else:
-        outfile = 1
-    selops.pop(0)
-else:
-    outfile = -1
-    print(f"{infile} -> {outfile}")
-"""
+        subprocess.run(cmd.split())
+    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input")
     parser.add_argument("output")
     parser.add_argument("--landscape", action="store_true")
-    parser.add_argument("--layout")
+    parser.add_argument("--layout", help="Like 2x1")
     parser.add_argument("--crop-margins", help="l t r b")
     parser.add_argument("--pages", help="pages to include") # how about a not-pages
     parser.add_argument("--gs-path", help="Path to gs executable", default="gs")
     parser.add_argument("--pdfcrop-path", help="Path to pdfcrop executable", default="pdfcrop")
     parser.add_argument("--pdfnup-path", help="Path to pdfnup executable", default="pdfnup")
+    parser.add_argument("--dry", action="store_true", help="Do a dry run")
     args = parser.parse_args()
 
+    # Queue up the selected operations
     ops = []
     if args.pages:
-        #ops.append(CMDS["gs"].format(args.gs_path, args.pages, args.input, args.output))
         ops.append("gs")
     if args.crop_margins:
-        #ops.append(CMDS["pdfcrop"].format(args.pdfcrop_path, args.crop_margins, args.input, args.output))
         ops.append("pdfcrop")
     if args.layout:
         ops.append("pdfnup")
-        if args.landscape:
-            landscape = args.landscape
-        #ops.append(CMDS["pdfnup"].format(args.pdfnup_path, args.layout, args.input, args.output, landscape))
-
 
     with tempfile.TemporaryDirectory() as tmpdir:
         tmpdirpath = pathlib.Path(tmpdir)
